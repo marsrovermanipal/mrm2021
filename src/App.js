2f93e89983697398f3637de6852307cbe040c52b
aaaaayushh <aayushshah1142@gmail.com>
2021-09-13 22:55:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Particles from "react-particles-js";
+import Navi from "./components/navbar/Navi";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/about-us">
+            {/* about us */}
+          </Route>
+          <Route exact path="/achievements">
+            {/* achievements */}
+          </Route>
+          <Route exact path="/contact-us">
+            {/* contact us */}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/Card.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import { useSpring, animated } from "react-spring";
+
+export default function Card({ children }) {
+  const ref = useRef();
+  const [isHovered, setHovered] = useState(false);
+  const [animatedProps, setanimatedProps] = useSpring(() => {
+    return {
+      xys: [0, 0, 1],
+      config: { mass: 10, tension: 400, friction: 40, precision: 0.00001 },
+    };
+  });
+  return (
+    <animated.div
+      ref={ref}
+      className="card"
+      onMouseEnter={() => setHovered(true)}
+      onMouseMove={({ clientX, clientY }) => {
+        const x =
+          clientX -
+          (ref.current.offsetLeft -
+            (window.scrollX || window.pageXOffset || document.body.scrollLeft));
+        const y =
+          clientY -
+          (ref.current.offsetTop -
+            (window.scrollY || window.pageYOffset || document.body.scrollTop));
+        const dampen = 50; // Lower the number the less rotation
+        const xys = [
+          -(y - ref.current.clientHeight / 2) / dampen,
+          (x - ref.current.clientWidth / 2) / dampen,
+          1.07,
+        ];
+
+        setanimatedProps({ xys: xys });
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+        // Set xys back to original
+        setanimatedProps({ xys: [0, 0, 1] });
+      }}
+      style={{
+        // If hovered we want it to overlap other cards when it scales up
+        zIndex: isHovered ? 2 : 1,
+        // Interpolate function to handle css changes
+        transform: animatedProps.xys.interpolate(
+          (x, y, s) =>
+            `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+        ),
+      }}
+    >
+      {children}
+    </animated.div>
+  );
+}
