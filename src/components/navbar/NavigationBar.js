40d40f6f76ec0549d7a27c5df2c9b7dc968faa5b
aaaaayushh <aayushshah1142@gmail.com>
2021-09-18 14:11:17 +0530

    navigation bar done

---
+++ src/components/navbar/NavigationBar.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import styles from "./Navi.module.css";
+export const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggle() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <div className="col-12">
+      <Navbar expand="md">
+        <NavbarBrand href="/">
+          <img src="/logo.png" alt="" className={`${styles.logo} img-fluid`} />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="col-12 justify-content-end" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about-us">About Us</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/competitions">Competitions</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/research">Research</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/alumni">Our Alumni</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,132 +1,59 @@
 import React from "react";
-import Navi from "../components/navbar/Navi";
 import Particles from "react-particles-js";
-import Card from "../components/card/Card";
+import { NavigationBar } from "../components/navbar/NavigationBar";
+
 export default function Landing() {
   return (
     <>
-      <Navi />
-      <div className="col-4">
-        <Card>
-          <div className="p-5">hello world</div>
-        </Card>
+      <NavigationBar />
+      <div className="col-12">
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 600,
+                density: {
+                  enable: true,
+                  value_area: 1500,
+                },
+              },
+              line_linked: {
+                enable: true,
+                opacity: 0.02,
+              },
+              move: {
+                direction: "right",
+                speed: 0.05,
+              },
+              size: {
+                value: 1,
+              },
+              opacity: {
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.05,
+                },
+              },
+            },
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: true,
+                  mode: "push",
+                },
+              },
+              modes: {
+                push: {
+                  particles_nb: 1,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
       </div>
+      <div className="col-4"></div>
     </>
   );
-}
-
-{
-  /* <Particles
-  params={{
-    fps_limit: 28,
-    particles: {
-      collisions: {
-        enable: false,
-      },
-      number: {
-        value: 200,
-        density: {
-          enable: false,
-        },
-      },
-      line_linked: {
-        enable: true,
-        distance: 30,
-        opacity: 0.4,
-      },
-      move: {
-        speed: 1,
-      },
-      opacity: {
-        anim: {
-          enable: true,
-          opacity_min: 0.05,
-          speed: 1,
-          sync: false,
-        },
-        value: 0.4,
-      },
-    },
-    polygon: {
-      enable: true,
-      scale: 0.5,
-      type: "inline",
-      move: {
-        radius: 10,
-      },
-      url: "/temp2.svg",
-      inline: {
-        arrangement: "equidistant",
-      },
-      draw: {
-        enable: true,
-        stroke: {
-          color: "rgba(255, 255, 255, .2)",
-        },
-      },
-    },
-    retina_detect: false,
-    interactivity: {
-      events: {
-        onhover: {
-          enable: true,
-          mode: "bubble",
-        },
-      },
-      modes: {
-        bubble: {
-          size: 6,
-          distance: 40,
-        },
-      },
-    },
-  }}
-/> */
-}
-{
-  /* <Particles
-  params={{
-    particles: {
-      number: {
-        value: 600,
-        density: {
-          enable: true,
-          value_area: 1500,
-        },
-      },
-      line_linked: {
-        enable: true,
-        opacity: 0.02,
-      },
-      move: {
-        direction: "right",
-        speed: 0.05,
-      },
-      size: {
-        value: 1,
-      },
-      opacity: {
-        anim: {
-          enable: true,
-          speed: 1,
-          opacity_min: 0.05,
-        },
-      },
-    },
-    interactivity: {
-      events: {
-        onclick: {
-          enable: true,
-          mode: "push",
-        },
-      },
-      modes: {
-        push: {
-          particles_nb: 1,
-        },
-      },
-    },
-    retina_detect: true,
-  }}
-/> */
 }
