74f6fd1b389b6b0f4ebd9c78a3b8d234998f5faa
kirtikediaa <kirtikediaa@github.com>
2021-10-06 22:10:05 +0530

    images updated

--- src/pages/Research/Research-data.js
+++ src/pages/Research/Research-data.js
@@ -2,8 +2,7 @@ const data = [
   {
     id: 1,
     title: 'BERT based Transformers lead the way in Extraction of Health Information from Social Media',
-    img:
-      'http://www.marsrovermanipal.com/image/research/bert.png',
+    img: "research/bert.png",
     event: "NAACL,2021",
     author: "Kumud Lakara, Siddarth R, Abhiraj Tiwari, Saisha Kashyap, Parthivi Choubay,Nishesh Singh, Sahil Khose",
   },
@@ -11,28 +10,28 @@ const data = [
     id: 2,
     title: 'Weed Density and Distribution Estimation for Precision Agriculture using Semi-Supervised Learning',
     img:
-      'http://www.marsrovermanipal.com/image/research/weeddensity.jpeg',
+      'research/weeddensity.jpeg',
     event: "IEEE Access,2021",
     author: "Shantam Shorewala, Siddarth R, Armaan Ashfaque, Ujjwal Verma",
   }, {
     id: 3,
     title: 'Wireless Sensor Network for Optimisation of Search and Rescue Management in Floods',
     img:
-      'http://www.marsrovermanipal.com/image/research/wireless.jpg',
+      'research/wireless.jpg',
     event: "IEEE Conecct,2021",
     author: "Harshil Bhatt, Pranesh G, Samarth Shankar, Shriyash Haralikar",
   }, {
     id: 4,
     title: 'Autonomous Control of Closed Chain Five Bar Active Suspension via Deep Reinforcement Learning',
     img:
-      'http://www.marsrovermanipal.com/image/research/fivebar.png',
+      'research/fivebar.png',
     event: "Under Review",
     author: "Nishesh Singh, Siddarth R, Jyotishka Duttagupta, Abhishek Sankar,Leander Stephen D'souza, Sanjay Singh",
   }, {
     id: 5,
     title: "Design and Analysis of Underactuated linear gripper for unstructured environments based on Chebyshev's Lambda Mechanism",
     img:
-      'http://www.marsrovermanipal.com/image/research/gripper.jpg',
+      'research/gripper.jpg',
     event: "Manipal Research Colloquium, Manipal Academy of Higher Education, April 2018",
     author: "K S Dhankhar, Md Suliaman, Shuvadeep Sarkar and Siril D Teja",
   },
