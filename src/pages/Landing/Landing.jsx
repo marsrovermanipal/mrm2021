cdf9e3f10af7b28889692e538cf1e6590c09a3dd
aaaaayushh <aayushshah1142@gmail.com>
2021-10-17 17:50:19 +0530

    landing fix

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -31,7 +31,7 @@ export default function Landing() {
   return (
     <>
       <div className={`col-12 ${styles.landing_anim}`}>
-        {/* <Particles
+        <Particles
           height="800px"
           params={{
             particles: {
@@ -83,51 +83,6 @@ export default function Landing() {
                 },
               },
             },
-          }}
-        /> */}
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 600,
-                density: {
-                  enable: true,
-                  value_area: 1500,
-                },
-              },
-              line_linked: {
-                enable: true,
-                opacity: 0.02,
-              },
-              move: {
-                direction: "right",
-                speed: 0.05,
-              },
-              size: {
-                value: 1,
-              },
-              opacity: {
-                anim: {
-                  enable: true,
-                  speed: 1,
-                  opacity_min: 0.05,
-                },
-              },
-            },
-            interactivity: {
-              events: {
-                onclick: {
-                  enable: true,
-                  mode: "push",
-                },
-              },
-              modes: {
-                push: {
-                  particles_nb: 1,
-                },
-              },
-            },
-            retina_detect: true,
           }}
         />
       </div>
