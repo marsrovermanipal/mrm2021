faf5f4e131d2427d5fdcad935a5878f0a5405a2a
kirtikediaa <kirtikediaa@github.com>
2021-11-01 17:22:30 +0530

    Sponsorship & Landing

--- src/pages/Sponsorship/BrochureData.js
+++ src/pages/Sponsorship/BrochureData.js
@@ -1,19 +1,16 @@
 const data = [
-    {
 
-        img: [
-            { src: "/brochure/1.jpg" },
-            { src: "/brochure/2.jpg" },
-            { src: "/brochure/3.jpg" },
-            { src: "/brochure/4.jpg" },
-            { src: "/brochure/5.jpg" },
-            { src: "/brochure/6.jpg" },
-            { src: "/brochure/7.jpg" },
-            { src: "/brochure/8.jpg" },
-            { src: "/brochure/9.jpg" },
-            { src: "/brochure/10.jpg" },
+    { src: "/brochure/1.jpg" },
+    { src: "/brochure/2.jpg" },
+    { src: "/brochure/3.jpg" },
+    { src: "/brochure/4.jpg" },
+    { src: "/brochure/5.jpg" },
+    { src: "/brochure/6.jpg" },
+    { src: "/brochure/7.jpg" },
+    { src: "/brochure/8.jpg" },
+    { src: "/brochure/9.jpg" },
+    { src: "/brochure/10.jpg" },
 
-        ],
-    }
+
 ]
 export default data
