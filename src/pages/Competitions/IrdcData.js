20d2fbe2de4a7e7e96efce36d3c33826a47f17df
aaaaayushh <aayushshah1142@gmail.com>
2021-11-06 20:05:01 +0530

    webp files added

--- src/pages/Competitions/IrdcData.js
+++ src/pages/Competitions/IrdcData.js
@@ -1,25 +1,23 @@
 const data = [
-    {
-        id: 1,
-        comp: "IRDC 2021",
-        info: "Mars Rover Manipal bagged the 3rd position out of 32 teams from four countries in the second edition of the International Rover Design Challenge (IRDC), which was hosted by Mars Society South Asia (MSSA).",
-        img: "comp/IRDC_2021.png",
-        // [
-        //     { src: "comp/IRDC_2021.png" },
-        //     { src: "comp/IRDC_TEAM.png" },
-        // ],
-        pos: "1st position",
-        website: "https://southasia.marssociety.org/irdc/",
-    },
-    {
-        id: 3,
-        comp: "IRDC 2020",
-        info: "Mars Rover Manipal bagged the 1st position in IRDC 2020. 28 teams from 7 countries had taken part in IRDC 2020. The result was announced on 15th August 2020 by Dr Robert Zubrin, the Founder and President of The Mars Society, USA. MRM scored 816.5 points for the submission, securing the first position by a margin of 15 points.",
-        img: 'comp/IRDCrover.jpg',
-        pos: "1st position",
-        website: "https://southasia.marssociety.org/irdc/",
-    },
-
-
-]
-export default data
+  {
+    id: 1,
+    comp: "IRDC 2021",
+    info: "Mars Rover Manipal bagged the 3rd position out of 32 teams from four countries in the second edition of the International Rover Design Challenge (IRDC), which was hosted by Mars Society South Asia (MSSA).",
+    img: "comp/IRDC_2021.webp",
+    // [
+    //     { src: "comp/IRDC_2021.png" },
+    //     { src: "comp/IRDC_TEAM.png" },
+    // ],
+    pos: "1st position",
+    website: "https://southasia.marssociety.org/irdc/",
+  },
+  {
+    id: 3,
+    comp: "IRDC 2020",
+    info: "Mars Rover Manipal bagged the 1st position in IRDC 2020. 28 teams from 7 countries had taken part in IRDC 2020. The result was announced on 15th August 2020 by Dr Robert Zubrin, the Founder and President of The Mars Society, USA. MRM scored 816.5 points for the submission, securing the first position by a margin of 15 points.",
+    img: "comp/IRDCrover.jpg",
+    pos: "1st position",
+    website: "https://southasia.marssociety.org/irdc/",
+  },
+];
+export default data;
