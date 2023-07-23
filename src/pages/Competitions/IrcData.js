d5c2cb47ece06da308cd088b0cd991e48a80ffa5
Saatwik Vasishtha <88363398+Teak-Rosewood@users.noreply.github.com>
2023-07-23 21:03:05 +0530

    Update IrcData.js

--- src/pages/Competitions/IrcData.js
+++ src/pages/Competitions/IrcData.js
@@ -1,7 +1,14 @@
 const data = [
-
     {
-        id: 4,
+        id: 1,
+        comp: "IRC 2023",
+        info: "Mars Rover Manipal placed 3rd out of 20 teams that qualified worldwide in the International Rover Challenge 2023",
+        pos: "3rd position",
+        website: "https://www.spaceroboticssociety.org/events/international-rover-challenge/",
+        img: "irc/image1.jpg",
+    },
+    {
+        id: 2,
         comp: "IRC 2019",
         info: "Indian Rover Challenge (IRC) 2019, the second edition, featured design challenges and technicalities, encouraging students from all over the world to provide solutions and move closer towards the next phases of space exploration. It was organized by the students of Mars Rover Manipal in association with Manipal Academy of Higher Education and the Mars Society, featuring a wide range of challenges and terrains which imitate the terrain of Mars.",
         pos: "Host",
@@ -9,7 +16,7 @@ const data = [
         img: "irc/image1.jpg",
     },
     {
-        id: 5,
+        id: 3,
         comp: "IRC 2018",
         info: "Mars Rover Manipal participated in IRC 2018 along with teams from all over Asia. The event was organized by ISTE VIT and Creative Labs at Vellore Insitute of Technology, Vellore from 6th January to 8th January. Mars Rover Manipal perfromed very well and were declared winners with a final score of 616 points out of 760. The team beat the second closest opponent by a margin of 129 points.",
         img: "irc/image3.webp",
