901b1c826cdb1d4ae36e024c02eccf6046f86f03
Mars Rover Manipal <electronics.marsrovermanipal@gmail.com>
2023-07-23 16:17:55 +0530

    Update AiData.js

--- src/pages/AboutUs/AiData.js
+++ src/pages/AboutUs/AiData.js
@@ -1,42 +1,10 @@
 const data = [
   {
     id: 1,
-    name: "Saipranav Vojjala",
-    img: "team-23/saipranav.webp",
-    linkedin: "http://www.linkedin.com/in/saipranav-vojjala-2770ab217",
-  },
-  {
-    id: 2,
-    name: "Vignesh S Iyer",
-    img: "team-23/vignesh.webp",
-    linkedin: "http://www.linkedin.com/in/vignesh-iyer-630b48217",
-  },
-  {
-    id: 3,
-    name: "Dishita Midha",
-    img: "team-23/dishita.webp",
-    linkedin: "https://www.linkedin.com/in/dishita-midha-960755214/",
-  },
-  {
-    id: 4,
-    name: "Aansh Basu",
-    img: "team-23/aansh.webp",
-    linkedin: "https://www.linkedin.com/in/aansh-basu-628310251",
-  },
-  {
-    id: 5,
     name: "Harsh Nandwani",
     img: "team-23/harshn.webp",
     linkedin: "https://www.linkedin.com/in/harsh-nandwani-57a238212",
   },
-  {
-    id: 6,
-    name: "Saatwik Vasishtha",
-    img: "team-23/saatwik.webp",
-    linkedin: "https://www.linkedin.com/in/saatwik-vasishtha-974a82148/",
-  },
-  
-  
 ];
 
 export default data;
