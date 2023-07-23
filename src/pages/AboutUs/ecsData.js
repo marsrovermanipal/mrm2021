0f43ec66f1a4d8340066a15c27cfff226907817c
Mars Rover Manipal <electronics.marsrovermanipal@gmail.com>
2023-07-23 16:19:24 +0530

    Update ecsData.js

--- src/pages/AboutUs/ecsData.js
+++ src/pages/AboutUs/ecsData.js
@@ -1,41 +1,16 @@
 const data = [
   {
     id: 1,
-    name: "Kanishk Kaushal",
-    img: "team-23/kanishk.webp",
-    linkedin: "https://www.linkedin.com/in/kanishk-kaushal-18a24520b",
-  },
-  {
-    id: 2,
     name: "Hrishikesh M Rao",
     img: "team-23/hrishikesh.webp",
     linkedin: "https://www.linkedin.com/in/hrishikesh-m-rao-443a1b22a",
   },
   {
-    id: 3,
+    id: 2,
     name: "Raj Nandini",
     img: "team-23/nandini.webp",
     linkedin: "https://www.linkedin.com/in/raj-nandini-6b85b6214",
-  },
-  {
-    id: 4,
-    name: "Swaraj Dangare",
-    img: "team-23/swaraj.webp",
-    linkedin: "https://www.linkedin.com/in/swaraj-dangare-952a21214",
-  },
-  {
-    id: 5,
-    name: "Tarush Sonawala",
-    img: "team-23/tarush.webp",
-    linkedin: "https://www.linkedin.com/in/tarush-sonawala-43bb03217",
-  },
-  {
-    id: 6,
-    name: "Uttiya Sangiri",
-    img: "team-23/uttiya.webp",
-    linkedin: "https://www.linkedin.com/in/uttiya-sangiri/",
-  },
-
+  }
 ];
 
 export default data;
