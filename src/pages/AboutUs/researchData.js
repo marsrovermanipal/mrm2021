9914442e68cf37a0f09b42f657de06172aa7b58e
Mars Rover Manipal <electronics.marsrovermanipal@gmail.com>
2023-07-23 16:24:59 +0530

    Update researchData.js

--- src/pages/AboutUs/researchData.js
+++ src/pages/AboutUs/researchData.js
@@ -1,65 +1,16 @@
 const data = [
   {
     id: 1,
-    name: 'Akshat Bhandari',
-    img: 'team-23/akshat.webp',
-    linkedin: "https://www.linkedin.com/in/akshat-bhandarixv/",
-  },
-  {
-    id: 2,
-    name: 'Pratinav Seth',
-    img: 'team-23/pratinav.webp',
-    linkedin: "https://www.linkedin.com/in/pratinav-seth",
-  },
-  {
-    id: 3,
-    name: 'Adil K',
-    img: 'team-23/adil.webp',
-    linkedin: "https://www.linkedin.com/in/adil-k-ba37161a2",
-  },
-  {
-    id: 4,
-    name: 'Aditya Kasliwal',
-    img: 'team-23/adityak.webp',
-    linkedin: "https://www.linkedin.com/in/aditya-kasliwal-982525227",
-  },
-  {
-    id: 5,
-    name: 'Ananya Gupta',
-    img: 'team-23/ananya.webp',
-    linkedin: "https://www.linkedin.com/in/ananya-gupta-982414204",
-  },
-  {
-    id: 6,
     name: 'Diya Mehta',
     img: 'team-23/diya.webp',
     linkedin: "https://www.linkedin.com/in/diya-mehta-5333b9238/",
   },
   {
-    id: 7,
+    id: 2,
     name: 'Ritika Ravichandran',
     img: 'team-23/ritika.webp',
     linkedin: "https://www.linkedin.com/mwlite/in/ritika-ravichandran-a565b9221",
-  },
-  {
-    id: 8,
-    name: 'Sanchit Singhal',
-    img: 'team-23/sanchit.webp',
-    linkedin: "https://www.linkedin.com/in/sanchit-singhal-851239220",
-  },
-  {
-    id: 10,
-    name: 'Sriya Rallabandi',
-    img: 'team-23/sriya.webp',
-    linkedin: "https://www.linkedin.com/in/sriya-rallabandi-1a614020a",
-  },
-  {
-    id: 9,
-    name: 'Saurabh Kumar Mishra',
-    img: 'team-23/saurabhm.webp',
-    linkedin: "https://www.linkedin.com/in/saurabh-mishra-5a0719218",
-  },
-
+  }
 ]
 
 
