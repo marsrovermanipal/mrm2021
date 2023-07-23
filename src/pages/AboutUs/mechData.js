4223cfec1ac0a209e39e86d455489082c8c93c22
Mars Rover Manipal <electronics.marsrovermanipal@gmail.com>
2023-07-23 16:24:05 +0530

    Update mechData.js

--- src/pages/AboutUs/mechData.js
+++ src/pages/AboutUs/mechData.js
@@ -1,65 +1,34 @@
 const data = [
   {
     id: 1,
-    name: "Ishayu Chakravorty",
-    img: "team-23/ishayu.webp",
-    linkedin: "https://www.linkedin.com/in/ishayu-chakravorty-699955216",
-  },
-  {
-    id: 2,
-    name: "Jeswanthi Tirupathi",
-    img: "team-23/jeswanthi.webp",
-    linkedin: "http://www.linkedin.com/in/jeswanthi-tirupathi-541695216",
-  },
-  {
-    id: 3,
-    name: "Saurabh Chaughule",
-    img: "team-23/saurabh.webp",
-    linkedin: "http://www.linkedin.com/in/saurabh-chaughule-740627200",
-  },
-  {
-    id: 4,
     name: "Aarushi Anuj Dharna",
     img: "team-23/aarushi.webp",
     linkedin: "https://www.linkedin.com/in/aarushi-dharna-84b1021b8",
   },
   {
-    id: 5,
+    id: 2,
     name: "Arjunshankar S Menon",
     img: "team-23/arjun.webp",
     linkedin: "https://www.linkedin.com/in/arjunshankar-menon-65951a221",
   },
   {
-    id: 6,
-    name: "Bharath Udupa",
-    img: "team-23/bharath.webp",
-    linkedin: "https://www.linkedin.com/in/bharath-u-7165281b2/",
-  },
-  {
-    id: 7,
-    name: "Harsh Phawade",
-    img: "team-23/harshp.webp",
-    linkedin: "https://www.linkedin.com/in/harsh-phawade-a94bb6250/",
-  },
-  {
-    id: 8,
+    id: 3,
     name: "Sanya Gupta",
     img: "team-23/sanya.webp",
     linkedin: "https://www.linkedin.com/in/sanya-gupta-790750251",
   },
   {
-    id: 9,
+    id: 4,
     name: "Shantanu Baranwal",
     img: "team-23/shantanu.webp",
     linkedin: "https://www.linkedin.com/in/shantanu-baranwal-a98a74250",
   },
   {
-    id: 10,
+    id: 5,
     name: "Shivansh Bansal",
     img: "team-23/shivansh.webp",
     linkedin: "https://www.linkedin.com/in/shivansh-bansal-04bbb2250",
-  },
-  
+  }
 ];
 
 export default data;
