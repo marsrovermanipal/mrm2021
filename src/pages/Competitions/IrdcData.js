6c96b4296aa015a0cef7345e1509b74971a29aa9
Saatwik Vasishtha <88363398+Teak-Rosewood@users.noreply.github.com>
2023-07-23 20:56:02 +0530

    Update IrdcData.js

--- src/pages/Competitions/IrdcData.js
+++ src/pages/Competitions/IrdcData.js
@@ -1,6 +1,14 @@
 const data = [
   {
     id: 1,
+    comp: "IRDC 2023",
+    info: "Mars Rover Manipal bagged the 4th position out of 20 teams from four countries in the fourth edition of the International Rover Design Challenge (IRDC), hosted by the Space Robotics Society (SPROS).",
+    img: "comp/irdc2023render.webp",
+    pos: "4th position",
+    website: "https://www.spaceroboticssociety.org/events/international-rover-challenge/",
+  },
+  {
+    id: 2,
     comp: "IRDC 2022",
     info: "Mars Rover Manipal bagged the 2nd position out of 24 teams from four countries in the third edition of the International Rover Design Challenge (IRDC), hosted by the Space Robotics Society (SPROS).",
     img: "comp/irdc2022render.webp",
@@ -8,7 +16,7 @@ const data = [
     website: "https://www.spaceroboticssociety.org/events/international-rover-challenge/",
   },
 {
-    id: 2,
+    id: 3,
     comp: "IRDC 2021",
     info: "Mars Rover Manipal bagged the 3rd position out of 32 teams from four countries in the second edition of the International Rover Design Challenge (IRDC), hosted by Mars Society South Asia (MSSA).",
     img: "comp/IRDC_20211.webp",
@@ -17,7 +25,7 @@ const data = [
   },
 
   {
-    id: 3,
+    id: 4,
     comp: "IRDC 2020",
     info: "Mars Rover Manipal bagged the 1st position in IRDC 2020. 28 teams from 7 countries had taken part in IRDC 2020. The result was announced on 15th August 2020 by Dr Robert Zubrin, the Founder and President of The Mars Society, USA. MRM scored 816.5 points for the submission, securing the first position by a margin of 15 points.",
     img: "comp/IRDCrover1.webp",
