34428a9fdb8222d026e7e691fd9aa0556e3f3fd4
Mars Rover Manipal <electronics.marsrovermanipal@gmail.com>
2023-07-23 16:25:26 +0530

    Update scienceData.js

--- src/pages/AboutUs/scienceData.js
+++ src/pages/AboutUs/scienceData.js
@@ -1,34 +1,10 @@
 const data = [
   {
     id: 1,
-    name: 'Varsha R',
-    img: 'team-22/varsha.webp',
-    linkedin: "https://www.linkedin.com/in/varsha-r-784891213/",
-  },
-  {
-    id: 2,
-    name: 'Mallika Shinkre',
-    img: 'team-23/mallika.webp',
-    linkedin: "https://www.linkedin.com/in/mallika-shinkre-1ab030261",
-  },
-  {
-    id: 3,
-    name: 'Pritha Jaipal',
-    img: 'team-23/pritha.webp',
-    linkedin: "https://www.linkedin.com/in/pritha-jaipal-058b85187/",
-  },
-  {
-    id: 4,
-    name: 'Rashi Goel',
-    img: 'team-23/rashi.webp',
-    linkedin: "https://www.linkedin.com/in/rashi-goel-5a385a251?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BNS2OpZpBSwONbswI4FceIQ%3D%3D",
-  },
-  {
-    id: 5,
     name: 'Saransh Andharmule',
     img: 'team-23/saransh.webp',
     linkedin: "https://www.linkedin.com/in/saransh-andharmule-a00952226",
-  },
+  }
 ]
 
 export default data
