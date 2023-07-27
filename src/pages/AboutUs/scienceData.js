55eb803aebaa93c0ace3b5e0148e09efd26651d4
Saatwik Vasishtha <88363398+Teak-Rosewood@users.noreply.github.com>
2023-07-27 08:55:07 +0530

    Update scienceData.js

--- src/pages/AboutUs/scienceData.js
+++ src/pages/AboutUs/scienceData.js
@@ -4,7 +4,13 @@ const data = [
     name: 'Saransh Andharmule',
     img: 'team-23/saransh.webp',
     linkedin: "https://www.linkedin.com/in/saransh-andharmule-a00952226",
-  }
+  },
+  {
+    id: 2,
+    name: 'Pritha Jaipal',
+    img: 'team-23/pritha.webp',
+    linkedin: "https://www.linkedin.com/in/pritha-jaipal-058b85187/",
+  },
 ]
 
 export default data
